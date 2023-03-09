648aba126022d39adece34ad51deda528214aeb9
Rajjit <2.0lairaj@gmail.com>
2023-03-09 15:44:47 +0530

    Updated 11.2

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from "./pages/Register";
 
-export default function App(){
-  return ;
+export default function App() {
+  return
+  {
+    <BrowserRouter>
+    <Routes>
+
+      {/* Route 1 */}
+      <Route path="/register" element={<Register />} />
+      {/* X Route 1 */}
+
+    </Routes>
+  </BrowserRouter >;
+  }
 }
