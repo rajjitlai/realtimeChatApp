46e647223841cafc2614d3e6e28fb5bd562146c6
Rajjit <2.0lairaj@gmail.com>
2023-03-09 16:41:52 +0530

    Updated 11.3

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Chat from "./pages/Chat";
 
 export default function App() {
   return
   {
     <BrowserRouter>
-    <Routes>
+      <Routes>
 
-      {/* Route 1 */}
-      <Route path="/register" element={<Register />} />
-      {/* X Route 1 */}
+        {/* Route 1 */}
+        <Route path="/register" element={<Register />} />
+        {/* X Route 1 */}
 
-    </Routes>
-  </BrowserRouter >;
-  }
+        {/* Route 2 */}
+        <Route path="/login" element={<Login />} />
+        {/* X Route 2 */}
+
+        {/* Route 3 */}
+        <Route path="/" element={<Chat />} />
+        {/* X Route 3 */}
+      </Routes>
+    </BrowserRouter >;
+  };
 }
--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CHat() {
+function Chat() {
   return <div>Chat</div>;
 }
 
