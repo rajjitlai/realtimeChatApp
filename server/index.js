07fc68d5f483fd258b2cf0806653a0c219b2bc3b
Rajjit <2.0lairaj@gmail.com>
2023-03-09 13:12:58 +0530

    Updated 5

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
+}).then(() => {
+          console.log("DB Connection Successful");
+}).catch((err) => {
+          console.log(err.message);
 });
 
 const server = app.listen(process.env.PORT, () => {
