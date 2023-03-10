42590aea37f5627cda2d55eb49036891bfaf5101
Rajjit <2.0lairaj@gmail.com>
2023-03-10 20:40:19 +0530

    Updated 13

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -1,7 +1,172 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import Logo from "../assets/rj.ico"; // change the logo after the development
+import { loginRoute } from "../utils/APIRoutes";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { password, username } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+      }
+      // useNavigate
+      navigate("/");
+    }
+  };
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleValidation = () => {
+    const { password, username } = values;
+    if (password === "") {
+      toast.error("Password is required", toastOptions);
+      return false;
+    } else if (username === "") {
+      toast.error("Username cannot be blank", toastOptions);
+    } else if (username.length === "") {
+      toast.error("Email and Password required", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>waathiapp</h1>
+          </div>
+          {/* <label htmlFor="username">Username</label> */}
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+            min="3"
+          />
+          {/* <label htmlFor="password">Password</label> */}
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+
+          <button type="submit">Login</button>
+          <span>
+            Don't have an account? &nbsp;
+            <Link to="/register">Register</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
 }
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #0e1419;
+
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: #fff;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: rgb(36 32 40 / 46%);
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #00fdea;
+      border-radius: 0.4rem;
+      color: #fff;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #38ff42;
+        outline: none;
+      }
+    }
+
+    button {
+      background-color: #00fdea;
+      padding: 1rem 2rem;
+      cursor: pointer;
+      color: #000;
+      border: none;
+      font-weight: 700;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        background-color: #38ff42;
+      }
+    }
+
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #38ff42;
+        text-decoration: none;
+        font-weight: 700;
+      }
+    }
+  }
+`;
 
 export default Login;
--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,3 +1,6 @@
 const host = "http://localhost:2023";
 
 export const registerRoute = `${host}/api/auth/register`;
+
+export const loginRoute = `${host}/api/auth/login`;
+
