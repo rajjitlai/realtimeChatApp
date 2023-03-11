cf169e32074bd038cf46116cd60cbddeff252eea
Rajjit <2.0lairaj@gmail.com>
2023-03-11 22:30:06 +0530

    Updated 14.5

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -11,10 +11,56 @@ export default function SetAvatar() {
   const api =
     "https://api.multiavatar.com/Starcrasher.png?apikey=YOUR_API_KEYYWE6ILg09AMzCF";
   const naviagate = useNavigate();
+  const [avatars, setAvatars] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const setProfilePicture = async () => {};
+  useEffect(async () => {
+    const data = [];
+    for (let i = 0; i < 4; i++) {
+      const image = await axios.get(
+        `${api}/${Math.round(Math.random() * 1000)}`
+      );
+      const buffer = new Buffer(image.data);
+      data.push(buffer.toString("base64"));
+    }
+    setAvatars(data);
+    setisLoading(false);
+  }, []);
 
   return (
     <>
-      <Container></Container>;
+      <Container>
+        <div className="title-container">
+          <h1>Pick an Avatar as your profile picture</h1>
+        </div>
+        <div key={index} className="avatars">
+          {avatars.map((avatar, index) => {
+            return (
+              <div
+                className={`avatar ${
+                  selectedAvatar === index ? "selected" : ""
+                }`}
+              >
+                <img
+                  src={`data:image/svg+xml;base64, ${avatar}`}
+                  alt="avatar"
+                  onClick={() => setSelectedAvatar(index)}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+      ;
       <ToastContainer />
     </>
   );
